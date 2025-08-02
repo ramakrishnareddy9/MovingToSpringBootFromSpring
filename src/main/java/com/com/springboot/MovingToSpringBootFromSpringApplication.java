bb9495f85f542ac4dc59f148041e390120f939c6
ramakrishnareddy9 <ramakrishnareddy.d9@gmail.com>
2025-08-02 21:12:06 +0530

    created springBoot

---
+++ src/main/java/com/com/springboot/MovingToSpringBootFromSpringApplication.java
@@ -0,0 +1,12 @@
+package com.com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovingToSpringBootFromSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovingToSpringBootFromSpringApplication.class, args);
+    }
+}
